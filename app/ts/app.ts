2f4282f52088ddf02429e5509ec6826835d4b08b
Rafael <rafaelbp92@gmail.com>
2020-09-27 19:41:26 -0300

    Initial commit

---
+++ app/ts/app.ts
@@ -0,0 +1,4 @@
+const controller = new NegociacaoController();
+
+//Usando jQuery
+$('.form').submit(controller.adiciona.bind(controller));
---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,12 @@
+class Negociacoes {
+
+  private _negociacoes : Negociacao[] = [];
+
+  add(negociacao : Negociacao) : void{
+    this._negociacoes.push(negociacao);
+  }
+
+  toArray():  Negociacao[] {
+    return [].concat(this._negociacoes);
+  }
+}
