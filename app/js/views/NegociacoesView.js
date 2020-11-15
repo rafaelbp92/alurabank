9acd28d8594ba96f9a404e93402d6ca16ba96722
Rafael <rafaelbp92@gmail.com>
2020-11-14 22:48:15 -0300

    Final course project

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -1,6 +1,17 @@
-class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var View_1, NegociacoesView;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
     <table class="table table-hover table-bordered">
         <thead>
             <tr>
@@ -13,7 +24,7 @@ class NegociacoesView extends View {
 
         <tbody>
           ${model.toArray().map(negociacao => {
-            return `
+                        return `
               <tr>
                 <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
                 <td>${negociacao.quantidade}</td>
@@ -21,12 +32,16 @@ class NegociacoesView extends View {
                 <td>${negociacao.volume}</td>
               </tr>
             `;
-        }).join('')}
+                    }).join('')}
         </tbody>
 
         <tfoot>
         </tfoot>
     </table>
     `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,17 +1,15 @@
-abstract class View<T> {
-
+export abstract class View<T> {
   private _elemento: JQuery;
 
   constructor(seletor: string) {
 
-      this._elemento = $(seletor);
+    this._elemento = $(seletor);
   }
 
   update(model: T) {
 
-      this._elemento.html(this.template(model));
+    this._elemento.html(this.template(model));
   }
 
- abstract  template(model: T): string;
-
+  abstract  template(model: T): string;
 }
